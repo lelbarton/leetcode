e1a01b67f541f8d05dc5e3bd29e98e545f162e4f
lelbarton <lauraelbarton@gmail.com>
2016-09-23 15:15:22 -0700

    Add comments to 238 solution

--- 238_product_of_array_except_self.java
+++ 238_product_of_array_except_self.java
@@ -8,13 +8,15 @@ public class Solution {
     public int[] productExceptSelf(int[] nums) {
         int[] result = new int[nums.length];
         int q = 1;
-        result[0] = 1;
+        result[0] = 1; // init to 1 so a zero doesn't cancel out quotient
 
+        // iterate forward through array, recording quotient so far
         for (int i = 0; i < nums.length - 1; i++) {
             q *= nums[i];
             result[i + 1] = q;
         }
 
+        // iterate backwards through array, recording final quotient
         q = 1;
         for (int j = nums.length - 1; j >= 1; j--) {
             q *= nums[j];
